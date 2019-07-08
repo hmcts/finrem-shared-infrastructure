2cd1b8cff02708bf59832e120e9e28a992e724b2
Chandra Korivi <Chandra.Korivi@HMCTS.NET>
2019-07-08 16:36:51 +0100

    FR-1146 Create ITHC environment for FR services.

--- key-vault.tf
+++ key-vault.tf
@@ -7,6 +7,7 @@ module "finrem-vault" {
   object_id = "${var.jenkins_AAD_objectId}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   product_group_object_id = "94ac8962-b614-441b-aa4c-9be878a6bf17"
+  common_tags = "${var.common_tags}"
 }
 
 output "vaultName" {
