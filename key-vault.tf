3c31eb4c99a9410b3e70b022eebbba2b06d772a0
TSeelig <thomas.seelig@HMCTS.NET>
2020-09-21 12:38:15 +0100

    upgrade azure resource manager

--- key-vault.tf
+++ key-vault.tf
@@ -1,16 +1,16 @@
 module "finrem-vault" {
-  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  source = "git@github.com:hmcts/cnp-module-key-vault?ref=azurermv2"
   name = "finrem-${var.env}"
-  product = "${var.product}"
-  env = "${var.env}"
-  tenant_id = "${var.tenant_id}"
-  object_id = "${var.jenkins_AAD_objectId}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
+  product = var.product
+  env = var.env
+  tenant_id = var.tenant_id
+  object_id = var.jenkins_AAD_objectId
+  resource_group_name = azurerm_resource_group.rg.name
   product_group_object_id = "94ac8962-b614-441b-aa4c-9be878a6bf17"
-  managed_identity_object_id = "${var.managed_identity_object_id}"
-  common_tags = "${local.tags}"
+  managed_identity_object_id = var.managed_identity_object_id
+  common_tags = local.tags
 }
 
 output "vaultName" {
-  value = "${module.finrem-vault.key_vault_name}"
+  value = module.finrem-vault.key_vault_name
 }
