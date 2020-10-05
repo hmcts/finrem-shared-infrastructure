aac46848f10561db815e9d76e79e64757180599f
gurarach <gurdeep.araich@hmcts.net>
2020-10-05 18:14:30 +0100

    added mi

--- main.tf
+++ main.tf
@@ -14,3 +14,8 @@ resource "azurerm_resource_group" "rg" {
 
   tags = local.tags
 }
+
+data "azurerm_user_assigned_identity" "finrem-identity" {
+  name                = "finrem-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
