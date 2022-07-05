33b9a4a4008054aa2804c4b0cc43477bb6681dc8
Mike Lemos <michael.lemos1@hmcts.net>
2022-07-05 15:13:52 +0100

    dfr-1103 added updated appinsights secret

--- application-insights.tf
+++ application-insights.tf
@@ -18,5 +18,5 @@ output "appInsightsInstrumentationKey" {
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
   value        = azurerm_application_insights.appinsights.connection_string
-  key_vault_id = module.finrem-vault.finrem-vault_id
+  key_vault_id = module.finrem-vault.key_vault_id
 }
