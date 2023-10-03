96e5d1a843e3a25a6e01d6be84e4c2896f1b315d
Hannah Smith <hannah.smith16@justice.gov.uk>
2023-10-03 15:03:37 +0100

    azurerm v

--- state.tf
+++ state.tf
@@ -8,7 +8,7 @@ terraform {
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.25"
+      version = "2.45.1"
     }
     random = {
       source = "hashicorp/random"
