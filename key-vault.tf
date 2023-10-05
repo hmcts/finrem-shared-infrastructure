cec39cdc03d50abb770ab70abfc7b72c3799e512
Hannah Smith <13995068+hannah38@users.noreply.github.com>
2023-10-05 10:09:39 +0100

    New Managed Identity, 1x terraform upgrade, renovate configured (#20)

--- key-vault.tf
+++ key-vault.tf
@@ -1,14 +1,15 @@
 module "finrem-vault" {
-  source = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
-  name = "finrem-${var.env}"
-  product = var.product
-  env = var.env
-  tenant_id = var.tenant_id
-  object_id = var.jenkins_AAD_objectId
-  resource_group_name = azurerm_resource_group.rg.name
-  product_group_name = "dcd_divorce"
+  source                     = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
+  name                       = "finrem-${var.env}"
+  product                    = var.product
+  env                        = var.env
+  tenant_id                  = var.tenant_id
+  object_id                  = var.jenkins_AAD_objectId
+  resource_group_name        = azurerm_resource_group.rg.name
+  product_group_name         = "dcd_divorce"
   managed_identity_object_id = var.managed_identity_object_id
-  common_tags = local.tags
+  create_managed_identity    = true
+  common_tags                = local.tags
 }
 
 output "vaultName" {
