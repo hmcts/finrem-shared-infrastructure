4e12988c1393175777d56bcb0c22a033085bb275
Hannah Smith <13995068+hannah38@users.noreply.github.com>
2023-10-06 14:48:23 +0100

    Remove secrets from tf (tf was overwriting their manually set values) (#24)

--- main.tf
+++ main.tf
@@ -16,15 +16,3 @@ resource "azurerm_resource_group" "rg" {
 
   tags = local.tags
 }
-
-resource "azurerm_key_vault_secret" "definition-importer-username" {
-   name = "definition-importer-username"
-   value = "definition-importer-username-placeholder"
-   key_vault_id = module.finrem-vault.key_vault_id
-}
-
-resource "azurerm_key_vault_secret" "definition-importer-password" {
-   name = "definition-importer-password"
-   value = "definition-importer-password-placeholder"
-   key_vault_id = module.finrem-vault.key_vault_id
-}
