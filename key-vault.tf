0d9f168176676b9d633ef70b027660b8bb2b0ac0
Rees Pozzi <reespozzi@icloud.com>
2023-10-06 15:40:00 +0100

    Cleanup old MI references

--- key-vault.tf
+++ key-vault.tf
@@ -7,7 +7,6 @@ module "finrem-vault" {
   object_id                  = var.jenkins_AAD_objectId
   resource_group_name        = azurerm_resource_group.rg.name
   product_group_name         = "dcd_divorce"
-  managed_identity_object_id = var.managed_identity_object_id
   create_managed_identity    = true
   common_tags                = local.tags
 }
