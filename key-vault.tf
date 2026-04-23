7ad2bbf0b11db243e7300cf8a16bcdff4c691626
manohar.nunna <manohar.nunna@hmcts.net>
2026-04-23 09:22:30 +0100

    DTSPO-30479: Add jenkins managed identity keyvault access

--- key-vault.tf
+++ key-vault.tf
@@ -1,3 +1,8 @@
+data "azurerm_user_assigned_identity" "jenkins" {
+  name                = "jenkins-${var.env}-mi"
+  resource_group_name = "managed-identities-${var.env}-rg"
+}
+
 module "finrem-vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   name                    = "finrem-${var.env}"
@@ -5,6 +10,7 @@ module "finrem-vault" {
   env                     = var.env
   tenant_id               = var.tenant_id
   object_id               = var.jenkins_AAD_objectId
+  jenkins_object_id       = data.azurerm_user_assigned_identity.jenkins.principal_id
   resource_group_name     = azurerm_resource_group.rg.name
   product_group_name      = "dcd_divorce"
   create_managed_identity = true
